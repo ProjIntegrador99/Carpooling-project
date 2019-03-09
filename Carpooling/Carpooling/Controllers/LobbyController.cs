613edb8da5fbd56ec5a7c2287d69367ab4fe9ef6
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-03-09 10:45:17 -0500

    LobbyController created

---
+++ Carpooling/Carpooling/Controllers/LobbyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpooling.Controllers
+{
+    public class LobbyController : Controller
+    {
+        public IActionResult Choose()
+        {
+            return View();
+        }
+    }
+}
